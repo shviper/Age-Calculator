08213d8a4928a18d585a38a50e8780a097f1ff11
sajjad-hasan-riyad <riyad01931@gmail.com>
2022-11-04 12:40:45 +0600

    add some extra option

--- script/main.js
+++ script/main.js
@@ -12,24 +12,38 @@ function birthCalculator() {
   // display div find
   const pName = document.getElementById("pName");
   const pOld = document.getElementById("pOld");
+  const pMonth = document.getElementById("pMonth");
   const pWeek = document.getElementById("pWeek");
   const pDay = document.getElementById("pDay");
+  const phour = document.getElementById("phour");
+  const pMinutes = document.getElementById("pMinutes");
+  const psecound = document.getElementById("psecound");
   // calculate birthday
-  const currentDate = (date.getFullYear() * 265) + ((date.getMonth() + 1) * 30) + date.getDay();
-  const usertDate = (pBirthYear * 265) + (pBirthMonth * 30) + pBirthDay;
+  const currentDate =
+    date.getFullYear() * 265 + (date.getMonth() + 1) * 30 + date.getDay();
+  const usertDate = pBirthYear * 265 + pBirthMonth * 30 + pBirthDay;
 
-  if(currentDate < usertDate){
-    alert("Invalid date formate!")
-  }else if (birthDay == "" || personName == ""){
-    alert("Enter your name and Birthday!")
-  }else {
-      document.getElementById("result").style.display="block";
-      const right = currentDate - usertDate;
-      // display information
-      pName.innerText = `name :${personName}`;
-      pOld.innerText = `old :${Math.floor(right / 265)} year ${Math.floor(right % 265 /30)} month ${Math.floor((right % 265) %30)} days`;
-      pWeek.innerText = `month :${Math.floor(right/30)}`
-      pDay.innerText = `days :${Math.floor(right)}`
+  if (currentDate < usertDate) {
+    alert("Invalid date formate!");
+  } else if (birthDay == "" || personName == "") {
+    alert("Enter your name and Birthday!");
+  } else {
+    document.getElementById("result").style.display = "block";
+    const right = currentDate - usertDate;
+    // display information
+    pName.innerHTML = `${personName}`;
+
+    pOld.innerHTML = `<b>${Math.floor(right / 265)}</b> year <b>${Math.floor(
+      (right % 265) / 30
+    )}</b> month <b>${Math.floor((right % 265) % 30)}</b> days`;
+
+    pMonth.innerHTML = `<b>${Math.floor(right / 30)}</b>`;
+
+    pWeek.innerHTML = `<b>${Math.floor(right / 7)}</b>`;
+    pDay.innerHTML = `<b>${Math.floor(right)}</b>`;
+    phour.innerHTML = `<b>${Math.floor(right * 24)}</b>`;
+    pMinutes.innerHTML = `<b>${Math.floor(right * 24 * 60)}</b>`;
+    psecound.innerHTML = `<b>${Math.floor(right * 24 * 60) * 60}</b>`;
   }
-  console.clear()
+  console.clear();
 }
